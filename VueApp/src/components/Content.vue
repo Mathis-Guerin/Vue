455f7f9b610941d13239612629dcaef4ff3a375a
mathisGuerin <mathis.guerin@idvroom.com>
2019-01-23 12:39:02 +0100

    Call user API and display users cards

--- VueApp/src/components/Content.vue
+++ VueApp/src/components/Content.vue
@@ -7,15 +7,24 @@
       <button @click="increment">+</button>
       <button @click="reset">Reset</button>
     </div>
+    <div class="content-users">
+      <UserCard v-bind:key="user.id" v-for="user in users" :user="user">
+      </UserCard>
+    </div>
   </div>
 </template>
 
 <script>
+import UserCard from './UserCard.vue'
 export default {
   data () {
     return {
-      counter: 0
+      counter: 0,
+      users: {}
     }
+  },
+  components: {
+    UserCard
   },
   methods: {
     increment () {
@@ -32,6 +41,13 @@ export default {
     isNegative: function () {
       return this.counter < 0
     }
+  },
+  mounted: function () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        this.users = json
+      })
   }
 }
 </script>
@@ -45,6 +61,8 @@ export default {
 }
 
 .content-compteur {
+  border-bottom: 2px solid #444;
+  padding-bottom: 20px;
   div {
     display: inline-block;
 
---
+++ VueApp/src/components/UserCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user-card">
+    <div>{{this.user.name}}</div>
+    <div>{{this.user.email}}</div>
+    <div>{{this.user.address.city}}</div>
+    <div>{{this.user.website}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['user']
+}
+</script>
+
+<style lang="scss" scoped>
+.user-card {
+  display: inline-block;
+  margin: 20px;
+  text-align: left;
+  width: 200px;
+}
+</style>
