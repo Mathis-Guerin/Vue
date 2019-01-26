31e6f1bbba47cc7e376e70249bdb420d99f04a3b
mathis-guerin <om.mathis@hotmail.fr>
2019-01-26 19:34:11 +0100

    Change userCard style

--- VueApp/src/components/UserCard.vue
+++ VueApp/src/components/UserCard.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="card">
+  <div class="card user-card">
     <div class="content">
-      <img class="right floated mini ui image" src="https://picsum.photos/200/?random">
-      <div class="header">{{this.user.name}}</div>
-      <div class="meta">{{this.user.email}}</div>
-      <div class="description">
-        <div>{{this.user.website}}</div>
-        <div>{{this.user.phone}}</div>
+      <div class="extra content right">
+        <button class="circular ui small white icon button">
+          <i @click="deleteUser(user)" class="x icon"></i>
+        </button>
       </div>
-    </div>
-    <div class="extra content right">
-      <button class="circular ui icon button red">
-        <i @click="deleteUser(user)" class="icon trash"></i>
+      <div class="image"><img class="right round" :src="imageUrl"></div>
+      <div class="user-name">{{this.user.name}}</div>
+      <div class="user-mail">{{this.user.email}}</div>
+      <button class="button-subscribe">
+        S'abonner
       </button>
     </div>
   </div>
@@ -24,12 +23,72 @@ export default {
     deleteUser (user) {
       this.$parent.deleteUser(user)
     }
+  },
+  computed: {
+    imageUrl: function () {
+      const randomId = this.user.id
+      return `http://lorempixel.com/g/200/200/people/${randomId}`
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.user-card {
+  font-family: Lato;
+}
+
+button {
+  &.white {
+    background-color: white !important;
+  }
+}
+
+.user-name {
+  margin: 20px 0 10px;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.user-mail {
+  color: #6f6f6f;
+}
+
+.image {
+  text-align: center;
+  margin: 0 auto;
+
+  img {
+    float: none !important;
+    margin: 0 auto;
+    height: 120px;
+  }
+}
+
 .right {
   text-align: right;
+}
+
+.round {
+  border-radius: 100%;
+}
+
+.button-subscribe {
+    appearance: none;
+    cursor: pointer;
+    display: block;
+    font-weight: 600;
+    padding: 8px 40px;
+    text-align: center;
+    text-transform: inherit;
+    text-overflow: ellipsis;
+    user-select: none;
+    white-space: nowrap;
+    background-color: #3897f0;
+    border: 1px solid #3897f0;
+    border-radius: 4px;
+    color: #fff;
+    position: relative;
+    margin: 10px auto 5px;
 }
 </style>
