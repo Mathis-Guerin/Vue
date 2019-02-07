7952155572445f8fc79ebaadf1aa4124f5a91db7
mathisGuerin <mathis.guerin@idvroom.com>
2019-02-07 17:16:50 +0100

    Add calcul logic and keep historic of results

--- VueApp/src/components/Calculator.vue
+++ VueApp/src/components/Calculator.vue
@@ -1,30 +1,40 @@
 <template>
-    <div class="Calculator">
-        <div class="Calculator-container">
-            <div class="touch screen">
-                <input :value="expression" type="text"/>
+    <div>
+        <div class="Calculator">
+            <div class="Calculator-container">
+                <div class="touch screen">
+                    <input :value="expression" type="text"/>
+                </div>
+                <div @click="copy()" class="touch result">
+                    {{result}}
+                </div>
+                <div @click="print(0)" class="touch touch-0">0</div>
+                <div @click="print(1)" class="touch touch-1">1</div>
+                <div @click="print(2)" class="touch touch-2">2</div>
+                <div @click="print(3)" class="touch touch-3">3</div>
+                <div @click="print(4)" class="touch touch-4">4</div>
+                <div @click="print(5)" class="touch touch-5">5</div>
+                <div @click="print(6)" class="touch touch-6">6</div>
+                <div @click="print(7)" class="touch touch-7">7</div>
+                <div @click="print(8)" class="touch touch-8">8</div>
+                <div @click="print(9)" class="touch touch-9">9</div>
+                <div @click="print('.')" class="touch touch-comma">.</div>
+                <div @click="calcul()" class="touch touch-equal">=</div>
+                <div @click="print('+')" class="touch touch-plus">+</div>
+                <div @click="print('-')" class="touch touch-moins">-</div>
+                <div @click="print('/')" class="touch touch-divide">/</div>
+                <div @click="print('*')" class="touch touch-multiply">x</div>
+                <div @click="print('(')" class="touch touch-paranthesis-left">(</div>
+                <div @click="print(')')" class="touch touch-paranthesis-right">)</div>
+                <div @click="erase()" class="touch touch-suppr"><i class="eraser icon"></i></div>
+                <div @click="clear()" class="touch touch-clear"><i class="trash icon"></i></div>
+                <div @click="square()" class="touch touch-square">²</div>
             </div>
-            <div class="touch result">
-                {{result}}
+        </div>
+        <div class="Calclator-historic">
+            <div class="Calclator-historic-wrapper" v-for="(history, index) in historic" :key="index">
+                <span @click="copyHistory(index)">{{history.expression}} = {{history.result}}</span>
             </div>
-            <div @click="print(0)" class="touch touch-0">0</div>
-            <div @click="print(1)" class="touch touch-1">1</div>
-            <div @click="print(2)" class="touch touch-2">2</div>
-            <div @click="print(3)" class="touch touch-3">3</div>
-            <div @click="print(4)" class="touch touch-4">4</div>
-            <div @click="print(5)" class="touch touch-5">5</div>
-            <div @click="print(6)" class="touch touch-6">6</div>
-            <div @click="print(7)" class="touch touch-7">7</div>
-            <div @click="print(8)" class="touch touch-8">8</div>
-            <div @click="print(9)" class="touch touch-9">9</div>
-            <div @click="print(',')" class="touch touch-comma">,</div>
-            <div @click="calcul()" class="touch touch-equal">=</div>
-            <div @click="print('+')" class="touch touch-plus">+</div>
-            <div @click="print('-')" class="touch touch-moins">-</div>
-            <div @click="print('/')" class="touch touch-divide">/</div>
-            <div @click="print('*')" class="touch touch-multiply">x</div>
-            <div class="touch touch-suppr">Suppr.</div>
-            <div class="touch touch-square">²</div>
         </div>
     </div>
 </template>
@@ -36,7 +46,8 @@ export default {
   data () {
     return {
       expression: '',
-      result: ''
+      result: '',
+      historic: []
     }
   },
   methods: {
@@ -45,6 +56,29 @@ export default {
     },
     calcul () {
       this.result = Math.eval(this.expression)
+      const historyObject = {
+        result: this.result,
+        expression: this.expression
+      }
+      this.historic.push(historyObject)
+      this.expression = ''
+    },
+    erase () {
+      this.expression = this.expression.substring(0, this.expression.length - 1)
+    },
+    copy () {
+      this.expression = this.expression.concat(this.result)
+    },
+    square () {
+      this.print('^2')
+    },
+    clear () {
+      this.expression = ''
+      this.result = ''
+    },
+    copyHistory (index) {
+      console.log(index)
+      this.expression = this.expression.concat(this.historic[index].result)
     }
   }
 }
@@ -52,15 +86,29 @@ export default {
 
 <style lang="scss" scoped>
 .Calculator {
+    display: inline-block;
     padding: 20px;
+}
+
+.Calclator-historic {
+    display: inline-block;
+    padding: 20px;
+    text-align: left;
+    vertical-align: top;
+
+    .Calclator-historic-wrapper {
+        font-size: 30px;
+        line-height: 40px;
+        cursor: pointer;
+    }
 }
 
 .Calculator-container {
     display: grid;
-    grid-template-columns: 60px 60px 60px 60px 60px;
+    grid-template-columns: 60px 60px 60px 60px 60px 60px;
     grid-template-rows: 180px 60px 60px 60px 60px 60px;
     width: 500px;
-    margin: 0 auto;
+    margin: 0 30px;
     background: #efefef;
     grid-gap: 10px;
     place-items: center;
@@ -89,6 +137,7 @@ export default {
             color: #F5F7FA;
             text-align: right;
             padding: 20px;
+            font-size: 35px;
         }
     }
 
@@ -96,6 +145,7 @@ export default {
         grid-column: 1 / 7;
         grid-row: 2 / 3;
         background-color: #fef4a9;
+        font-size: 34px;
     }
 
     @for $i from 1 through 3 {
@@ -165,7 +215,7 @@ export default {
     }
 
     .touch-equal {
-        grid-column: 3 / 6;
+        grid-column: 3 / 7;
         grid-row: 6 / 6;
     }
 
@@ -174,13 +224,24 @@ export default {
     }
 
     .touch-comma, .touch-plus, .touch-moins,
-    .touch-multiply, .touch-divide, .touch-square {
+    .touch-multiply, .touch-divide, .touch-square,
+    .touch-paranthesis-left, .touch-paranthesis-right {
         background-color: #4bc2c5;
     }
 
-    .touch-suppr {
+    .touch-suppr, .touch-clear {
         background-color: #ea7070;
         font-size: 18px;
+    }
+
+    .touch-paranthesis-left {
+        grid-column: 6 / 7;
+        grid-row: 5 / 5;
+    }
+
+    .touch-paranthesis-right {
+        grid-column: 6 / 7;
+        grid-row: 4 / 4;
     }
 }
 </style>
